9a4a2151b5331c78bdcdf837c51bf60abaeca62b
= <=>
2019-04-22 20:36:20 +0400

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p className='verdict success'>The water would boil.</p>;
+    }
+    return <p className='verdict fail'>The water would not boil.</p>;
+}
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Water Boild Detect'
+        }
+    }
+
+    render() {
+        const title = this.state.title;
+        return (
+            <header className='header'>
+                <h1 className='app-title'>{title}</h1>
+            </header>
+        );
+    }
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value);
+    }
+
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange}
+                />
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {
+            temperature: '',
+            scale: 'c'
+        };
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({
+            scale: 'c',
+            temperature
+        });
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({
+            scale: 'f',
+            temperature
+        })
+    }
+
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div className='calc-wrapper'>
+                <TemperatureInput
+                    scale='c'
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange}
+                />
+                <TemperatureInput
+                    scale='f'
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange}
+                />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <div className='container'>
+        <Header />
+        <Calculator />
+    </div>,
+    document.getElementById('root')
+);
